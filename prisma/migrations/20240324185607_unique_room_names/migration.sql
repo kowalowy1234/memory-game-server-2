dc4d72261b62a8a685a768c5a3de4997050db2d1
kowalowy1234 <kowalowy1234@gmail.com>
2024-03-30 17:39:02 +0100

    remove orm cause it won't be needed after all

--- prisma/migrations/20240324185607_unique_room_names/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[room_name]` on the table `Room` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Room_room_name_key" ON "Room"("room_name");
